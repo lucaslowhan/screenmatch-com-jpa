b6e8977c371115d9343489e220c8f9addc8638ad
lucaslowhan <lucas.lowhan@hotmail.com>
2025-07-15 08:11:15 -0300

    att principal and serie

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,9 +1,23 @@
 package br.com.alura.screenmatch.model;
 
 public enum Categoria {
-    ACAO,
-    ROMANCE,
-    COMEDIA,
-    DRAMA,
-    CRIME
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+    Categoria(String categoriaOmdb){
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text){
+        for(Categoria categoria : Categoria.values()){
+            if(categoria.categoriaOmdb.equalsIgnoreCase(text)){
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida.");
+    }
 }
