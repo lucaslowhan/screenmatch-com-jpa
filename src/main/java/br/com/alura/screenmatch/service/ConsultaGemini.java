2a84cdf350c302ac35b8f66ed5779f9ee49e255d
lucaslowhan <lucas.lowhan@hotmail.com>
2025-07-19 21:10:58 -0300

    Adicionando banco de dados

--- src/main/java/br/com/alura/screenmatch/service/ConsultaGemini.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaGemini.java
@@ -1,15 +1,17 @@
 package br.com.alura.screenmatch.service;
 
+
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 
 public class ConsultaGemini {
-    private static final String GEMINI_API_KEY = "AIzaSyBIArNnDiQiTyiSBwZilrSs3GzkO2l98kQ";
+    private static final String GEMINI_API_KEY = System.getenv("GEMINI_API_KEY");
 
     public static String obterTraducao(String texto){
         ChatLanguageModel gemini = GoogleAiGeminiChatModel.builder()
                 .apiKey(GEMINI_API_KEY)
-                .modelName("gemini-2.5-flash")
+                .modelName("gemini-1.5-flash")
                 .maxOutputTokens(1000)
                 .temperature(0.7)
                 .build();
